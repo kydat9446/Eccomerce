3b5a5eeb56e48c181d49a2722bdc211e8b4dcdc0
kydat9446 <0306181306@caothang.edu.vn>
2021-01-01 16:12:04 +0700

    Add project files.

---
+++ Eccomerce/Areas/Admin/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eccomerce.Areas.Admin.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public int Catid { get; set; }
+        [ForeignKey("Catid")]
+        public virtual TypeProduct TypeP { get; set; }
+    } 
+}
---
+++ Eccomerce/Areas/Admin/Models/TypeProduct.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Eccomerce.Areas.Admin.Models
+{
+    public class TypeProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Status { get; set; }
+    }
+}
