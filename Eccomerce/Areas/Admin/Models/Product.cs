373eaae6e3c721602c9bc13ebf58bf0040e9b9f4
Kan1989bd <crimsondakuto@gmail.com>
2021-01-13 20:03:51 +0700

    FixValidation

--- Eccomerce/Areas/Admin/Models/Product.cs
+++ Eccomerce/Areas/Admin/Models/Product.cs
@@ -12,8 +12,7 @@ namespace Eccomerce.Areas.Admin.Models
         public int Id { get; set; }
         [Required]
         [StringLength(50, MinimumLength = 2)]
-        public string Name { get; set; }
-        [Required]        
+        public string Name { get; set; } 
         public string Image { get; set; }
         [Required]        
         public int Price { get; set; }
